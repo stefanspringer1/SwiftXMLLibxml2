6dfe8d7a765fa10fc5b0a07bfdbf1c940e0d11fe
Stefan Springer <stefan.springer@stefanspringer.com>
2021-05-10 23:13:56 +0200

    import FoundationXML conditionally

--- Sources/SwiftXML/Document.swift
+++ Sources/SwiftXML/Document.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationXML)
+import FoundationXML
+#endif
 
 public class XMLDocument {
     public init(document: String, catalog: String? = nil) {
--- Sources/SwiftXML/EntityCorrection.swift
+++ Sources/SwiftXML/EntityCorrection.swift
@@ -1,4 +1,7 @@
 import Foundation
+#if canImport(FoundationXML)
+import FoundationXML
+#endif
 
 class EntityDeclarationsGetter: NSObject, XMLParserDelegate {
     
